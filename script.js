a8a8ccab30e4d3f3e7349206161846cc6159a5ea
Khushbooji88 <khushbookhushboo20093@gmail.com>
2024-10-22 09:45:57 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Your script here.
 <body>
 	<!-- Content of the page goes here -->
-    <h1>Welcome to My Webpage</h1>
+    <h1>Welcome to My Webpage</h2>
     <p>This is a paragraph of text on my webpage.</p>
     <button onclick="myFunction()">Click me</button>
 
